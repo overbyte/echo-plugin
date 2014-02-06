cc3cc381faff9304f0b8876a5d0e57c40bff73fa
overbyte <allandt@gmail.com>
2014-02-06 11:50:46 +0000

    first commit

---
+++ com.publicislondon.plugins.echo/www/echo.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var echo = function () {
+	console.log('Conversation Checker is here');
+
+	window.echo = function(str, callback) {
+		console.log('echoing: ' + str);
+        cordova.exec(callback, function(err) {
+            callback('Nothing to echo.');
+        }, "Echo", "echo", [str]);
+    };
+};
+
+
+module.exports = echo;
