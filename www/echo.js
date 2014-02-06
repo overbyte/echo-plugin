21de8869215575e9eef4bdf70a3ba78d849b1674
overbyte <allandt@gmail.com>
2014-02-06 13:10:18 +0000

    removed adding window.echo to echo function

--- www/echo.js
+++ www/echo.js
@@ -3,12 +3,12 @@
 var echo = function () {
 	console.log('Conversation Checker is here');
 
-	window.echo = function(str, callback) {
+	//window.echo = function(str, callback) {
 		console.log('echoing: ' + str);
         cordova.exec(callback, function(err) {
             callback('Nothing to echo.');
         }, "Echo", "echo", [str]);
-    };
+    //};
 };
 
 
